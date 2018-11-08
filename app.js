386b94ce68a73b2fc76c6e7ba2eb6987b0bfd158
Kiran <kiranmai9417@gmail.com>
2018-11-08 15:28:04 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello World")
+console.log('Starting app.;')
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js')
+var res = notes.add(2,3);
+console.log(res);
+ var user = os.userInfo();
+
+fs.appendFile('helloWorld.txt',`hello ${user.username} !`,(err) => {
+    if (err) throw (err)
+});
+
+
